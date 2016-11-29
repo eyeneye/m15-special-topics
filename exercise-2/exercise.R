c88ec3440cc4b7b6748827ffd2783c0c9380554e
eyeneye <nathanbombardier@d-173-250-163-200.dhcp4.washington.edu>
2016-11-29 15:09:02 -0800

    added stuff

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,12 +1,13 @@
 # Introductory example using the housing data used here: 
 # http://www.r2d3.us/visual-intro-to-machine-learning-part-1/
+install.packages('rpart')
 library(rpart)
-library(rpart.plot)
+# library(rpart.plot)
 
 # Read in data
-setwd('~/Documents/info-201/m15-special-topics/exercise-2')
+setwd('~/Documents/school/Fall2016/INFO201/m15-special-topics/exercise-2')
 homes <- read.csv('data/housing-data.csv')
-
+View(homes)
 # Function to compare values
 AssessFit <- function(model, data = homes, outcome = 'in_sf') {
   predicted <- predict(model, data, type='class')
@@ -33,25 +34,19 @@ for(i in 1:100) {
   # Hint: http://stackoverflow.com/questions/17200114/how-to-split-data-into-training-testing-sets-using-sample-function-in-r-program
   # 1. Create training and testing datasets by sampling 75% of your data from your `homes` dataframe.
   train.indicies <- sample(seq_len(nrow(homes)), size = sample.size)
-  training.data <- homes[train.indicies,]
-  test.data <- homes[-train.indicies,]
-  
+  training.data<- homes[train.indicies]
+  test.data <- homes[-train.indicies]
   # 2. Pass your **training data** to the `rpart` function to run a simple classification operation
-  basic.fit <- rpart(in_sf ~ ., data = training.data, method="class")
-  
+  basic.fit <- rpart(in_sf ~ ., data = training.data, method = 'class')
   # 3. Pass your results to the `AssessFit` function to assess the fit
-  assessment <- AssessFit(basic.fit, data=test.data)
-  
+  assessment <- AssessFit(basic.fit, data = test.data)
   # 4. Store your assessment in the `basic.fits` vector
-  basic.fits <- c(basic.fits, assessment)
+  basic.fit <- c(basic.fits, assessment)
 }
 
 # 5. Make a histogram of your `basic.fits` vector
 hist(basic.fits)
-
 # 6. Take the mean of your `basic.fits` vector
-mean(basic.fits)
 
 
 # 7. Pass your most recent model to the `rpart.plot` function to graph it
-rpart.plot(basic.fit)
